3209f1a4eeb65be16e2ddae4afaf5a8b7ed400bd
katajak <101673344+katajak@users.noreply.github.com>
2022-04-04 18:59:28 +0300

    Remove useless variables

--- src/index.py
+++ src/index.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((display_width, display_height))
 paddle1_color = [255, 255, 255]
 paddle2_color = [255, 255, 255]
 
+paddle1_speed = 6
+paddle2_speed = 6
+
 paddle1 = Paddle(paddle1_color, 8, 80)
 paddle1.rect.x = 20
 paddle1.rect.y = int(display_height/2 - 40)
@@ -22,25 +25,23 @@ all_sprites = pygame.sprite.Group()
 all_sprites.add(paddle1)
 all_sprites.add(paddle2)
 
-clock = pygame.time.Clock()
+fps = pygame.time.Clock()
 
-running = True
-
-while running:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            exit()
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        paddle1.move_up(4)
+        paddle1.move_up(paddle1_speed)
     if keys[pygame.K_s]:
-        paddle1.move_down(4)
+        paddle1.move_down(paddle1_speed)
 
     if keys[pygame.K_UP]:
-        paddle2.move_up(4)
+        paddle2.move_up(paddle2_speed)
     if keys[pygame.K_DOWN]:
-        paddle2.move_down(4)
+        paddle2.move_down(paddle2_speed)
 
     screen.fill([20, 20, 20])
 
@@ -48,4 +49,4 @@ while running:
 
     pygame.display.flip()
 
-    clock.tick(60)
+    fps.tick(60)
