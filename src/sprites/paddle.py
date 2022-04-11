faefc34b4ef12f96fbfe3ffc9e624251a781f2ad
katajak <101673344+katajak@users.noreply.github.com>
2022-04-11 13:14:55 +0300

    Cant move paddle offscreen

--- src/sprites/paddle.py
+++ src/sprites/paddle.py
@@ -7,10 +7,15 @@ class Paddle(pygame.sprite.Sprite):
         self.image = pygame.Surface([width, height])
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         self.rect = self.image.get_rect()
+        self.height = height
 
     def move_down(self, px):
         self.rect.y += px
+        if self.rect.y > 600 - self.height:
+            self.rect.y = 600 - self.height
 
     def move_up(self, px):
         self.rect.y -= px
+        if self.rect.y < 0:
+            self.rect.y = 0
         
