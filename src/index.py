38057cd3363b6135878a7ab256de48f43db2efa8
katajak <101673344+katajak@users.noreply.github.com>
2022-04-11 20:26:28 +0300

    Better code

--- src/index.py
+++ src/index.py
@@ -14,12 +14,12 @@ paddle2_color = [255, 255, 255]
 paddle1_speed = 6
 paddle2_speed = 6
 
-paddle1 = Paddle(paddle1_color, 8, 80)
-paddle1.rect.x = 20
+paddle1 = Paddle(paddle1_color, 8, 80, display_height)
+paddle1.rect.x = 30
 paddle1.rect.y = int(display_height/2 - 40)
  
-paddle2 = Paddle(paddle2_color, 8, 80)
-paddle2.rect.x = display_width - 30
+paddle2 = Paddle(paddle2_color, 8, 80, display_height)
+paddle2.rect.x = display_width - 30 - 8
 paddle2.rect.y = int(display_height/2 - 40)
 
 all_sprites = pygame.sprite.Group()
--- src/sprites/paddle.py
+++ src/sprites/paddle.py
@@ -1,18 +1,19 @@
 import pygame
 
 class Paddle(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
+    def __init__(self, color, width, height, display_height):
         super().__init__()
 
         self.image = pygame.Surface([width, height])
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         self.rect = self.image.get_rect()
         self.height = height
+        self.display_height = display_height
 
     def move_down(self, px):
         self.rect.y += px
-        if self.rect.y > 600 - self.height:
-            self.rect.y = 600 - self.height
+        if self.rect.y > self.display_height - self.height:
+            self.rect.y = self.display_height - self.height
 
     def move_up(self, px):
         self.rect.y -= px
